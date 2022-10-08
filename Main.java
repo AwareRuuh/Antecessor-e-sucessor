34dff9cfbed77bba4d9c492a31fae24481677515
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-08 13:17:00 +0000

    Dessa vez perguntei se o usuário gostaria de dar seu valor ou ver o valor que eu já havia declarado.

--- Main.java
+++ Main.java
@@ -4,14 +4,25 @@ class Main {
   
   public static void main(String[] args) {
   Scanner ler = new Scanner(System.in);
-    int numInteiro, sucessor, antecessor;
+    int numInteiro, sucessor, antecessor, numint=1, suces, ant;
     int a = 2;
-  System.out.print("Informe o seu valor: ");
-  numInteiro = ler.nextInt();
-    sucessor = numInteiro + 1;  
-    antecessor = numInteiro - 1;  
-  System.out.println("O sucessor do seu valor: " + sucessor);  
-  System.out.println("O antecessor do seu valor: " + antecessor);
-  System.out.println("O meu valor é: "+a);
+    System.out.println("Você quer inserir seu valor? [1] Sim ou [2] Não: ");
+    int DecisionInt = ler.nextInt();
+    String Decision = "";
+
+     if (DecisionInt == 1) {
+        System.out.print("Informe o seu valor: ");
+        numInteiro = ler.nextInt();
+        sucessor = numInteiro + 1;  
+        antecessor = numInteiro - 1;  
+         System.out.println("O sucessor do seu valor: " + sucessor);  
+         System.out.println("O antecessor do seu valor: " + antecessor);
+    } else if (DecisionInt == 2) {
+        System.out.println("O meu valor é: "+a);
+        suces = numint + 1;
+        ant = numint - 1;
+         System.out.println("O sucessor do meu valor é: " + suces);  
+         System.out.println("O antecessor do meu valor é: " + ant);
+    }
 }
   }
